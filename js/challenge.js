2428280e258ac6691f67574c937c3ce66bc49b97
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-17 09:57:11 +0300

    Update challenge.js

--- js/challenge.js
+++ js/challenge.js
@@ -4,9 +4,9 @@ function photoName() {
   var name = prompt("Please enter your name");
    
   //remove additional whitespaces
-  
+  nane = name.trim()
   //replace the space between names with underscore
- 
+  name = name.replace("","_")
   //change name to all lowercase
     alert("Your photo name is " + name + ".jpg")
     
